1e2f2ee29e478898a9e8c5849b9bc006abcbc9a1
Ben Xavier <153337331+NoodleQuark@users.noreply.github.com>
2024-12-05 11:35:46 -0800

    Update main.java
    comments

--- main/main.java
+++ main/main.java
@@ -1,5 +1,8 @@
 package main;
 
+//this is the launch class that the launch .json is set to
+//not sure if it really works tho
+// might need to add gampanel.run()
 public class Main {
     public static void main(String[] args) {
         GamePanel gamePanel = new GamePanel();
