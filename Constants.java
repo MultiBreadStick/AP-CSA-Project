e53367409fda5320a1932fb9aea829c2b190a528
CedarD12 <drakecedar@gmail.com>
2024-12-06 11:46:36 -0800

    More constants!!!

--- Constants.java
+++ Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     public static final double CRAB_SPEED = 0.75;
     public static final double CRAB_ATTACK_MULTIPLIER = 1.25;
     public static final String CRAB_NAME = "Crab";
+    public static final int CRAB_MAX = 3;
 
     //Goblin
     public static final int GOBLIN_HEALTH = 50;
@@ -17,6 +18,7 @@ public class Constants {
     public static final double GOBLIN_SPEED = 1.25;
     public static final double GOBLIN_ATTACK_MULTIPLIER = 1.0;
     public static final String GOBLIN_NAME = "Goblin";
+    public static final int GOBLIN_MAX = 5;
 
     //Kiffe (yoked)
     public static final int KIFFE_HEALTH = 100;
@@ -24,6 +26,7 @@ public class Constants {
     public static final double KIFFE_SPEED = 1.0;
     public static final double KIFFE_ATTACK_MULTIPLIER = 1.25;
     public static final String KIFFE_NAME = "Kiffe";
+    public static final int KIFFE_MAX = 3;
 
     //Giant
     public static final int GIANT_HEALTH = 200;
@@ -31,6 +34,7 @@ public class Constants {
     public static final double GIANT_SPEED = 0.75;
     public static final double GIANT_ATTACK_MULTIPLIER = 1.25;
     public static final String GIANT_NAME = "Giant";
+    public static final int GIANT_MAX = 2;
 
     //Big rock
     public static final int BIG_ROCK_HEALTH = 150;
@@ -45,9 +49,16 @@ public class Constants {
     public static final double SWORD_MULTIPLIER = 1.5;
     public static final double SWORD_WEAPON_SPEED = 1.5;
     public static final String SWORD_NAME = "Sword";
+    public static final boolean SWORD_IS_RANGED = false;
 
     //Hammer
     public static final double HAMMER_MULTIPLIER = 3.0;
     public static final double HAMMER_WEAPON_SPEED = 0.75;
     public static final String HAMMER_NAME = "Hammer";
+    public static final boolean HAMMER_IS_RANGED = false;
+
+    //Bow
+    public static final double BOW_MULTIPLIER = 1.25;
+    public static final double BOW_WEAPON_SPEED = 1.0;
+    
 }
