572d44371b2cdc79ae38aaadbd9c48d19d37b660
Ben Xavier <153337331+NoodleQuark@users.noreply.github.com>
2024-12-09 15:02:34 -0800

    Update KeyHandler.java restored keyTyped

--- main/KeyHandler.java
+++ main/KeyHandler.java
@@ -1,15 +1,18 @@
 package main;
 
-import mainObjects.PlayerStats;
-import mainObjects.Constants;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener{
 	
 	public boolean upPressed, downPressed, leftPressed, rightPressed;
-	public PlayerStats player; 
 
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+    
     //checks which WASD keys are being pressed
 	@Override
 	public void keyPressed(KeyEvent e) {
@@ -30,11 +33,13 @@ public class KeyHandler implements KeyListener{
 			rightPressed = true;
 
 		}
+		
+		
+		
 	}
 
     //checks which WASD keys are released
     //we might not need this but it was easy to copy and paste
-	//Pretty sure its not -Cedar
 	@Override
 	public void keyReleased(KeyEvent e) {
 		int code = e.getKeyCode();
@@ -56,5 +61,4 @@ public class KeyHandler implements KeyListener{
 		}
 	}
 	
-
 }
