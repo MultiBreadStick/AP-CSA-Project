5eef5fad2849c04a0edb85207a18aeab42d35e03
CedarD12 <drakecedar@gmail.com>
2024-12-11 11:46:12 -0800

    Update Room.java

--- mainObjects/Room.java
+++ mainObjects/Room.java
@@ -1,16 +1,18 @@
-package mainObjects;
+package mainObjects;\
+
 
 
 public class Room {
     private int level;
     private boolean inRoom;
     private String type;
+    private int numEnemies;
 
-    public Room(int level, boolean inRoom, String type) {
+    public Room(int level, boolean inRoom, String type, int numEnemies) {
         this.level = level;
         this.inRoom = inRoom;
         this.type = type;
-
+        this.numEnemies = numEnemies;
     }
 
     //get set
