f3956fe328afa4e0915ae62f4c92ce73d6511729
CedarD12 <drakecedar@gmail.com>
2024-12-12 12:23:58 -0800

    Update Attack.java will error don't worry I know

--- mainObjects/Attack.java
+++ mainObjects/Attack.java
@@ -3,19 +3,22 @@ package mainObjects;
 public class Attack {
     //instance declaration
     private int damage;
-    private String name;
     private Weapon weapon;
     private int width;
     private int length;
     
     //constructors
-    public Attack(int damage, String name, Weapon weapon, int width, int length) {
+    public Attack(int damage, Weapon weapon, int width, int length) {
         this.damage = damage;
         this.name = name;
         this.weapon = weapon;
         this.width = width;
         this.length = length;
     }
+    public Attack(int damage, int width, int length) {
+        this.damage = damage;
+    }
+    
     //getter/setters
     public int getEnemyAttackDamage(Enemy enemy) {
         return (int)(damage * weapon.getMultiplier() * enemy.getAttackMultiplier() * (int)(Math.random() * 5));
