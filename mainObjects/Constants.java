d41c7d51a27b18565f8a6f828d0241c51ad87e63
CedarD12 <drakecedar@gmail.com>
2024-12-12 12:20:54 -0800

    Update Constants.java

--- mainObjects/Constants.java
+++ mainObjects/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     public static final int MAX_Y = 9*120;
 
     //Crab
+    public static final Attack CRAB_ATTACK = new Attack(20, );
     public static final Enemy crab = new Enemy(100, 0.75, 3, 1.25, "Crab", true);
     public static final int CRAB_MIN = 2;
     public static final int CRAB_MAX = 3;
